b5411982551696fc0a2b1ec360dc0a04245ee7ab
PurpleGamerSyd <sydneychien2006@gmail.com>
2021-12-29 21:17:00 -0800

    sleepy

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 80
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('img'))
+
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`)
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(port, () => {
